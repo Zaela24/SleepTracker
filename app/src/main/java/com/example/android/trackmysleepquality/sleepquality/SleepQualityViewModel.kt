47ed2d3ac20c386afa063cd8a9aaf5056198bd1e
Zaela24 <zaela.marie.rose@gmail.com>
2020-04-18 20:37:46 -0500

    Added SleepQualityViewModel & Factory & click handlers

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -15,3 +15,41 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.*
+
+class SleepQualityViewModel(private val sleepNightKey: Long = 0L,
+                            val database: SleepDatabaseDao) : ViewModel() {
+    private val viewModelJob = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+    val navigateToSleepTracker: LiveData<Boolean?>
+            get() = _navigateToSleepTracker
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onSetSleepQuality(quality: Int) {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                // if null, return from withContext() call
+                val tonight = database.get(sleepNightKey) ?: return@withContext
+                tonight.sleepQuality = quality
+                database.update(tonight)
+            }
+            // Done updating database, return after indicating to navigate to SleepTrackerFragment
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel() // Terminates all coroutines when onCleared() is called
+    }
+}
